4463e20c22a2b43ba713950bf47cdb65bb635692
KangDo96 <as20190101@hotmail.com>
2021-05-15 13:18:56 +0800

    updated logo sit

--- src/containers/TheSidebar.vue
+++ src/containers/TheSidebar.vue
@@ -4,15 +4,16 @@
     :minimize="minimize"
     :show="show"
     @update:show="(value) => $store.commit('set', ['sidebarShow', value])"
+    style="background: rgba(0, 40, 77, 1)"
   >
     <CSidebarBrand class="d-md-down-none" to="/">
-      <CIcon 
-        class="c-sidebar-brand-full" 
-        name="logo" 
-        size="custom-size" 
-        :height="35" 
-        viewBox="0 0 556 134"
-      />
+<!--      <CIcon -->
+<!--        class="c-sidebar-brand-full" -->
+<!--        name="logo" -->
+<!--        size="custom-size" -->
+<!--        :height="35" -->
+<!--        viewBox="0 0 556 134"-->
+<!--      />-->
       <CIcon 
         class="c-sidebar-brand-minimized" 
         name="logo" 
@@ -20,6 +21,8 @@
         :height="35" 
         viewBox="0 0 110 134"
       />
+      <img style="width: 30px; height: 30px; margin-right: 20px" id="u69_img" class="img " src="http://iot.flsiot.com:202/pub/images/框架/u68.svg">
+      <span>场站管理</span>
     </CSidebarBrand>
 
     <CRenderFunction flat :content-to-render="$options.nav"/>
